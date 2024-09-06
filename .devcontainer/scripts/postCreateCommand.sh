061c7f7d940a76b4df37433da0a2b979f7ed0db0
Tom <tomecrepont@gmail.com>
2024-09-06 17:47:15 +0000

    initial commit

---
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -0,0 +1,5 @@
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_USER
+git config --global init.defaultBranch main
+git config --global pull.rebase false
+git config --global push.autoSetupRemote true
