8088611e5ab3fe9504a2f37127852eb4714ee44b
Tom <tomecrepont@gmail.com>
2024-09-11 13:11:24 +0000

    first setup of kamal

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM golang:1.22
+
+ADD go.mod go.sum /
+
+RUN go mod download
+
+WORKDIR /app
+
+COPY . .
+
+ENV GO_ENV=production
+
+RUN go build -o /app/web cmd/web/main.go
+
+EXPOSE 3000
+CMD ["/app/web"]
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,8 +9,11 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./internal/views/index.html")
 }
 
+func healthCheckHandler(w http.ResponseWriter, r *http.Request){}
+
 func main() {
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+	http.HandleFunc("/up", healthCheckHandler)
 	http.HandleFunc("/", rootHandler)
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
