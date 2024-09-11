54a6a43b20d413682b2386387793f6323d957271
Tom <tomecrepont@gmail.com>
2024-09-11 09:32:15 +0000

    create a dead simple web server that serve the index html

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./internal/views/index.html")
+}
+
+func main() {
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+	http.HandleFunc("/", rootHandler)
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
