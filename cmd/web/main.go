98524131e11145c007df3fdd8fd61a0a98c1ab0d
Tom <tomecrepont@gmail.com>
2024-09-16 07:54:00 +0000

    base skeleton of the subscription page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,17 +4,25 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func healthCheckHandler(c echo.Context) error {
 	return c.NoContent(http.StatusOK)
 }
 
+func createSubscriptionHandler(c echo.Context) error {
+	return c.File("internal/views/subscription/created.html")
+}
+
 func main() {
 	e := echo.New()
-
+	e.Use(middleware.Logger())
 	e.File("/", "internal/views/index.html")
 	e.GET("/up", healthCheckHandler)
 	e.Static("/public", "public")
+
+	e.POST("/subscription", createSubscriptionHandler)
+	
 	e.Logger.Fatal(e.Start(":3001"))
 }
