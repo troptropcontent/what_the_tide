8d89ddb309baec6324ccf532aa6a755123bacda6
Tom <tomecrepont@gmail.com>
2024-09-16 13:04:24 +0000

    create the gorm Port model

--- internal/models/port.go
+++ internal/models/port.go
@@ -1,47 +1,13 @@
 package models
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"os"
+	"gorm.io/gorm"
 )
 
 const PortsConfigFile = "config/ports.json"
 
 type Port struct {
-	Name string `json:"name"`
-	Id   int    `json:"id"`
-}
-
-type PortsConfig struct {
-	Ports []Port `json:"ports"`
-}
-
-func (portsConfig *PortsConfig) LoadFromJson(path string) error {
-	jsonFile, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	byteValue, _ := io.ReadAll(jsonFile)
-	json.Unmarshal(byteValue, portsConfig)
-
-	return nil
-}
-
-func (portsConfig *PortsConfig) FindPort(id int) (requestedPort Port, err error) {
-	// TODO : Use memoization here
-	for _, port := range portsConfig.Ports {
-		if port.Id == id {
-			requestedPort = port
-
-			break
-		}
-	}
-
-	if requestedPort.Id == 0 {
-		return requestedPort, fmt.Errorf("no ports found for this ID : %d", id)
-	}
-
-	return requestedPort, nil
+	gorm.Model
+	ExternalId int
+	Name       string
 }
