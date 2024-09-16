6fc2dadc06fd671df98d08144b93aa0638fa27d0
Tom <tomecrepont@gmail.com>
2024-09-16 07:53:13 +0000

    add gorm columns to ports

--- database/migrations/1726432172_create_ports_table.up.sql
+++ database/migrations/1726432172_create_ports_table.up.sql
@@ -1,4 +1,7 @@
 CREATE TABLE IF NOT EXISTS ports (
     id INT PRIMARY KEY NOT NULL,
+    created_at datetime NOT NULL,
+    updated_at datetime NOT NULL,
+    deleted_at datetime NOT NULL,
     name varchar
 )
