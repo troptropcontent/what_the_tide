8474f9d155e588b0c1a10a86fc9f4893a182a1fb
Tom <tomecrepont@gmail.com>
2024-09-25 12:43:14 +0000

    use different database depending on environment

--- database/database.go
+++ database/database.go
@@ -1,6 +1,10 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/labstack/gommon/log"
 	"github.com/troptropcontent/what_the_tide/config"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -8,10 +12,19 @@ import (
 
 var DB *gorm.DB
 
-const DBPath = "database/database.db"
+const BaseDbPath = "database/database"
+
+func DbPath() string {
+	var env string
+	if env = os.Getenv("WHAT_THE_TIDE_ENV"); env == "" {
+		log.Fatalf("environment variable WHAT_THE_TIDE_ENV seems not to be set")
+	}
+
+	return fmt.Sprintf("%s/%s_%s.db", config.Root(), BaseDbPath, env)
+}
 
 func MustInit() {
-	db, err := gorm.Open(sqlite.Open(config.Root()+"/"+DBPath), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open(DbPath()), &gorm.Config{})
 	if err != nil {
 		panic("error while connecting to database")
 	}
